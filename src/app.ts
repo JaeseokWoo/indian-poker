81656076cd614dcfc1d04a1cbfeabfefa422603a
JaeseokWoo <JaeseokW@gmail.com>
2022-10-31 00:28:03 +0900

    [FEAT] app.ts 생성

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-const express = require("express");
+import express from 'express';
+
+const app = express();
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  console.log('here');
+  res.json('test');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기 중');
+});
