c960bb2043d577748bf5eaaac77da031ebd2f51f
JaeseokWoo <JaeseokW@gmail.com>
2022-12-11 11:05:07 +0900

    [DELETE] test log

--- src/routes/room.ts
+++ src/routes/room.ts
@@ -3,18 +3,15 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  console.log('roomRouter / get');
   res.send(req.app.get('rooms'));
 });
 
 router.post('/', (req, res, next) => {
-  console.log('roomRouter / post');
   const { title } = req.body;
   const { color: owner } = req.session as any;
   const rooms = req.app.get('rooms');
 
   req.app.set('rooms', [...rooms, { id: rooms.length + 1, title, owner }]);
-  console.log(req.app.get('rooms'));
   res.send(true);
 });
 
