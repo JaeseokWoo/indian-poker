03a32c4eae5885a6c37635dfa61ee68ac0071ad9
JaeseokWoo <JaeseokW@gmail.com>
2023-03-01 17:06:25 +0900

    [REAT] 안쓰는 모듈 제거

--- front-end/src/rooms/component/Button.tsx
+++ front-end/src/rooms/component/Button.tsx
@@ -1,4 +1,4 @@
-import React, { MouseEventHandler } from 'react';
+import { MouseEventHandler } from 'react';
 
 export default function Button({ title, onClick }: { title: string; onClick: MouseEventHandler<HTMLButtonElement> }) {
   return <button onClick={onClick}>{title}</button>;
